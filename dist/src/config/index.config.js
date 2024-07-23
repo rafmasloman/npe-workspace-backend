c70d609446c772bf832a26ae35059c8924eb1ef9
rafmasloman <raflymasloman20@gmail.com>
2024-07-23 16:48:00 +0800

    fix : add dist to gitignore

--- dist/src/config/index.config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const CONFIG = {
-    PORT: process.env.PORT,
-    DATABASE_URL: process.env.DATABASE_URL,
-    JWT_KEY: process.env.JWT_KEY,
-};
--- dist/src/constants/auth.constant.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.HashPassword = void 0;
-exports.HashPassword = {
-    SALT_ROUND: 10,
-};
