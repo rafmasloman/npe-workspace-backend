4f803be1a9bc3998874ae376ec1755fa1ddcfd24
rafmasloman <raflymasloman20@gmail.com>
2024-08-15 19:58:31 +0800

    fix : change schema usr validation

--- src/utils/schema.utils.ts
+++ src/utils/schema.utils.ts
@@ -2,10 +2,10 @@ import Joi from 'joi';
 
 export const userValidationSchema = Joi.object({
   username: Joi.string().alphanum().min(6).max(20).required(),
-  password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+  password: Joi.string().min(6).pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
   email: Joi.string().email().required(),
-  firstname: Joi.string().max(100).required(),
-  lastname: Joi.string().max(100).required(),
+  firstname: Joi.string().required(),
+  lastname: Joi.string().required(),
   role: Joi.string().valid('ADMIN', 'STAFF', 'PROJECT_MANAGER').required(),
   memberId: Joi.string(),
 });
